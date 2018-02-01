1060ef4c59a23a6ea74417c7a06f9a5695a33f6a
Kirill Khudyakov <hudiakov@gmail.com>
2018-02-01 18:11:49 +0300

    Initial Commit

---
+++ Diagrammer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diagrammer
+//
+//  Created by Kirill Khudyakov on 01.02.18.
+//  Copyright © 2018 adeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
